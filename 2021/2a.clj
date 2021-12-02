86001b817b2a4647211813e25cd8f71f41cc236f
Kevin Conner <connerk@gmail.com>
2021-12-02 00:11:51 -0500

    Day 2 part 1

---
+++ 2021/2a.clj
@@ -0,0 +1,20 @@
+(def lines (clojure.string/split
+            (slurp "2.txt")
+            #"\n"))
+
+(def steps (map
+            (fn [line]
+                (let [[direction distance] (clojure.string/split line #"\s")]
+                    [direction (Integer. distance)]))
+            lines))
+
+(def end (reduce
+            (fn [[h d] [direction distance]]
+                (case direction
+                    "forward" [(+ h distance) d]
+                    "up" [h (- d distance)]
+                    "down" [h (+ d distance)]))
+            [0 0]
+            steps))
+
+(apply * end)
