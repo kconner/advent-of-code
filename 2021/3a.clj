704e37b3b74c2926ae8ab0d7d7421307a73d9909
Kevin Conner <connerk@gmail.com>
2021-12-03 14:04:11 -0500

    Day 3 part 1

---
+++ 2021/3a.clj
@@ -0,0 +1,31 @@
+(def lines (clojure.string/split (slurp "3.txt") #"\n"))
+
+(defn bits-for-line [line]
+    (map #(= \1 %) line))
+
+(def bits-by-line (map (comp bits-for-line reverse seq) lines))
+
+(def bits-by-place (apply map list bits-by-line))
+
+(defn counts-by-value [values]
+    (apply hash-map
+        (mapcat (fn [[value list]] [value (count list)])
+            (group-by identity values))))
+
+(def counts-by-bit-by-place (map counts-by-value bits-by-place))
+
+(def gamma-bits
+    (map (fn [counts] (< (counts false) (counts true)))
+        counts-by-bit-by-place))
+
+(def epsilon-bits (map not gamma-bits))
+
+(defn number-for-bits [bits]
+    (let [bit-vector (apply vector bits)]
+        (first (reduce
+            (fn [[sum power], index]
+                [(+ sum (if (bit-vector index) power 0)) (* power 2)])
+            [0 1]
+            (range (count bit-vector))))))
+
+(apply * (map number-for-bits [gamma-bits epsilon-bits]))
