472dc41145e8cf42f0aa57cb490539f715ab734c
Kevin Conner <connerk@gmail.com>
2021-12-04 02:16:57 -0500

    Day 4, part 1

--- 2021/4a.clj
+++ 2021/4a.clj
@@ -2,14 +2,14 @@
   (clojure.string/split (slurp "4.txt") #"\s*\n\n\s*"))
 
 (def draws
-  (into () (map #(Integer. %))
-        (clojure.string/split (first paragraphs) #",")))
+  (map #(Integer. %)
+       (clojure.string/split (first paragraphs) #",")))
 
-(defn square-for-string [string]
-  {:value (Integer. string), :marked false})
+(defn square-from-string [string]
+  {:value (Integer. string)})
 
 (defn row-from-line [line]
-  (into [] (map square-for-string)
+  (into [] (map square-from-string)
         (clojure.string/split line #"\s+")))
 
 (defn board-from-paragraph [paragraph]
@@ -30,23 +30,38 @@
         indices))
 
 (defn has-board-won [board]
-  (or (has-board-won-by-axis board (fn [major minor] [major minor]))   ;; row
-      (has-board-won-by-axis board (fn [major minor] [minor major])))) ;; column
+  (or (has-board-won-by-axis board (fn [major minor] [major minor]))
+      (has-board-won-by-axis board (fn [major minor] [minor major]))))
 
-;; (defn find-winning-board [boards draws]
-;;   (if-let (some has-board-won boards)
+(defn boards-after-draw [boards draw]
+  (into [] conj
+        (map (fn [board]
+               (into []
+                     (map (fn [line]
+                            (into []
+                                  (map (fn [square]
+                                         (if (= (:value square) draw)
+                                           (assoc square :marked true)
+                                           square)))
+                                  line)))
+                     board))
+             boards)))
 
+(defn find-winner [boards prior-draw draws]
+  (if (empty? draws) :no-winner
+      (if-let [winning-board (some (fn [board] (and (has-board-won board) board)) boards)]
+        {:board winning-board, :final-draw prior-draw}
+        (find-winner
+         (boards-after-draw boards (first draws))
+         (first draws)
+         (rest draws)))))
 
-(def aboard
-  (-> boards
-      (update-in [0 1 0 :marked] (constantly true))
-      (update-in [0 1 2 :marked] (constantly true))
-      (update-in [0 1 2 :marked] (constantly true))
-      (update-in [0 1 3 :marked] (constantly true))
-      (update-in [0 1 4 :marked] (constantly true))))
+(defn score-for-winner [winner]
+  (* (:final-draw winner)
+     (->> (:board winner)
+          (apply concat)
+          (remove :marked)
+          (map :value)
+          (reduce +))))
 
-aboard
-
-(has-board-won (first aboard))
-
-;; (get-in (first aboard) [0 0 :marked])
+(score-for-winner (find-winner boards nil draws))
