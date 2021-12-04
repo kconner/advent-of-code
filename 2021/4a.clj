40194aa974f4060671658d84a5b0968a75fd5fb9
Kevin Conner <connerk@gmail.com>
2021-12-04 01:27:18 -0500

    Detect when a board has won

--- 2021/4a.clj
+++ 2021/4a.clj
@@ -1,7 +1,7 @@
 (def paragraphs
   (clojure.string/split (slurp "4.txt") #"\s*\n\n\s*"))
 
-(def draw-order
+(def draws
   (into () (map #(Integer. %))
         (clojure.string/split (first paragraphs) #",")))
 
@@ -20,4 +20,33 @@
   (into [] (map board-from-paragraph)
         (rest paragraphs)))
 
-boards
+(def indices ((comp range count first) boards))
+
+(defn has-board-won-by-axis [board make-key-path]
+  (some (fn [major]
+          (every? (fn [minor]
+                    (get-in board (conj (make-key-path major minor) :marked)))
+                  indices))
+        indices))
+
+(defn has-board-won [board]
+  (or (has-board-won-by-axis board (fn [major minor] [major minor]))   ;; row
+      (has-board-won-by-axis board (fn [major minor] [minor major])))) ;; column
+
+;; (defn find-winning-board [boards draws]
+;;   (if-let (some has-board-won boards)
+
+
+(def aboard
+  (-> boards
+      (update-in [0 1 0 :marked] (constantly true))
+      (update-in [0 1 2 :marked] (constantly true))
+      (update-in [0 1 2 :marked] (constantly true))
+      (update-in [0 1 3 :marked] (constantly true))
+      (update-in [0 1 4 :marked] (constantly true))))
+
+aboard
+
+(has-board-won (first aboard))
+
+;; (get-in (first aboard) [0 0 :marked])
