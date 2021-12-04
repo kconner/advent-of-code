62c9dd36c563c34fd4d0c288ca11c9b6a97ed539
Kevin Conner <connerk@gmail.com>
2021-12-04 00:42:33 -0500

    Make storage for board marks

--- 2021/4a.clj
+++ 2021/4a.clj
@@ -1,18 +1,23 @@
-(def paragraphs (clojure.string/split (slurp "4.txt") #"\s*\n\n\s*"))
+(def paragraphs
+  (clojure.string/split (slurp "4.txt") #"\s*\n\n\s*"))
 
 (def draw-order
   (into () (map #(Integer. %))
         (clojure.string/split (first paragraphs) #",")))
 
+(defn square-for-string [string]
+  {:value (Integer. string), :marked false})
+
 (defn row-from-line [line]
-  (into [] (map #(Integer. %))
+  (into [] (map square-for-string)
         (clojure.string/split line #"\s+")))
 
 (defn board-from-paragraph [paragraph]
   (into [] (map row-from-line)
         (clojure.string/split paragraph #"\s*\n\s*")))
 
-(def boards (into [] (map board-from-paragraph)
-                  (rest paragraphs)))
+(def boards
+  (into [] (map board-from-paragraph)
+        (rest paragraphs)))
 
 boards
