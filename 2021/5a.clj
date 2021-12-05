f0e4e6569b2d7a7cff7f9130dcca4f59667059df
Kevin Conner <connerk@gmail.com>
2021-12-05 16:31:00 -0500

    Use reductions to build covered point lists

--- 2021/5a.clj
+++ 2021/5a.clj
@@ -5,11 +5,11 @@
                (or (= x0 x1) (= y0 y1))))
      (mapcat (fn [[x0 y0 x1 y1]]
                (let [step [(compare x1 x0) (compare y1 y0)]]
-                 (reduce (fn [[point :as all] _] (conj all (map + point step)))
-                         (list [x0 y0])
-                         (->> (map #(Math/abs (- %1 %2)) [x0 y0] [x1 y1])
-                              (apply max)
-                              range)))))
+                 (reductions (fn [point _] (map + point step))
+                             [x0 y0]
+                             (->> (map #(Math/abs (- %1 %2)) [x0 y0] [x1 y1])
+                                  (apply max)
+                                  range)))))
      (reduce (fn [map key]
                (update map key (fn [value]
                                  (inc (or value 0))))) {})
