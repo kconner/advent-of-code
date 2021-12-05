76d39e733efb6e22eec43a9a863168b6e5b8663a
Kevin Conner <connerk@gmail.com>
2021-12-05 14:54:26 -0500

    Reuse shorter general solution for part 1

--- 2021/5a.clj
+++ 2021/5a.clj
@@ -1,18 +1,15 @@
 (->> (clojure.string/split (slurp "5.txt") #"\n")
-     (map (fn [line] (clojure.string/split line #" -> ")))
-     (map (fn [point-strings]
-            (->> point-strings
-                 (map (fn [string] (clojure.string/split string #",")))
-                 (map (fn [ordinate-strings]
-                        (mapv #(Integer. %) ordinate-strings))))))
-     (filter (fn [[[x0 y0] [x1 y1]]]
-               (or (= x0 x1)
-                   (= y0 y1))))
-     (map sort)
-     (mapcat (fn [[[x0 y0] [x1 y1]]]
-               (for [x (range x0 (inc x1))
-                     y (range y0 (inc y1))]
-                 [x y])))
+     (map (fn [line] (clojure.string/split line #" -> |,")))
+     (map (fn [ordinate-strings] (mapv #(Integer. %) ordinate-strings)))
+     (filter (fn [[x0 y0 x1 y1]]
+               (or (= x0 x1) (= y0 y1))))
+     (mapcat (fn [[x0 y0 x1 y1]]
+               (let [step [(compare x1 x0) (compare y1 y0)]]
+                 (reduce (fn [[point :as all] _] (conj all (map + point step)))
+                         (list [x0 y0])
+                         (->> (map #(Math/abs (- %1 %2)) [x0 y0] [x1 y1])
+                              (apply max)
+                              range)))))
      (reduce (fn [map key]
                (update map key (fn [value]
                                  (inc (or value 0))))) {})
