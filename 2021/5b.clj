d37ed4cf09abe91cfd9b8de572560846ac792b6d
Kevin Conner <connerk@gmail.com>
2021-12-05 14:45:48 -0500

    Build list without multplying

--- 2021/5b.clj
+++ 2021/5b.clj
@@ -2,15 +2,13 @@
      (map (fn [line] (clojure.string/split line #" -> |,")))
      (map (fn [ordinate-strings] (mapv #(Integer. %) ordinate-strings)))
      (mapcat (fn [[x0 y0 x1 y1]]
-               (let [xstep (compare x1 x0)
-                     ystep (compare y1 y0)]
-                 (for [step (->> (map #(Math/abs (- %1 %2))
-                                      [x0 y0] [x1 y1])
-                                 (apply max)
-                                 inc
-                                 range)]
-                   [(+ x0 (* step xstep))
-                    (+ y0 (* step ystep))]))))
+               (let [step [(compare x1 x0) (compare y1 y0)]]
+                 (reduce (fn [[point :as list] _]
+                           (conj list (map + point step)))
+                         (list [x0 y0])
+                         (->> (map #(Math/abs (- %1 %2)) [x0 y0] [x1 y1])
+                              (apply max)
+                              range)))))
      (reduce (fn [map key]
                (update map key (fn [value]
                                  (inc (or value 0))))) {})
