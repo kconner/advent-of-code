3bf83a262751be0c66e67323e9dcba3bbddfd6d8
Kevin Conner <connerk@gmail.com>
2021-12-05 13:05:59 -0500

    Use mapcat

--- 2021/5a.clj
+++ 2021/5a.clj
@@ -9,11 +9,10 @@
                (or (= x0 x1)
                    (= y0 y1))))
      (map sort)
-     (map (fn [[[x0 y0] [x1 y1]]]
-            (for [x (range x0 (+ 1 x1))
-                  y (range y0 (+ 1 y1))]
-              [x y])))
-     (apply concat)
+     (mapcat (fn [[[x0 y0] [x1 y1]]]
+               (for [x (range x0 (+ 1 x1))
+                     y (range y0 (+ 1 y1))]
+                 [x y])))
      (reduce (fn [map key]
                (update map key (fn [value]
                                  (inc (or value 0))))) {})
