5b61ffaeb9e16a41a2a4c636f99014ac40c9006f
Kevin Conner <connerk@gmail.com>
2021-12-05 14:25:55 -0500

    Flatten point pair data

--- 2021/5b.clj
+++ 2021/5b.clj
@@ -1,14 +1,14 @@
 (->> (clojure.string/split (slurp "5.txt") #"\n")
-     (map (fn [line] (clojure.string/split line #" -> ")))
-     (map (fn [point-strings]
-            (->> point-strings
-                 (map (fn [string] (clojure.string/split string #",")))
-                 (map (fn [ordinate-strings]
-                        (mapv #(Integer. %) ordinate-strings))))))
-     (mapcat (fn [[[x0 y0] [x1 y1]]]
+     (map (fn [line] (clojure.string/split line #" -> |,")))
+     (map (fn [ordinate-strings] (mapv #(Integer. %) ordinate-strings)))
+     (mapcat (fn [[x0 y0 x1 y1]]
                (let [xstep (compare x1 x0)
                      ystep (compare y1 y0)]
-                 (for [step (range (inc (apply max (map #(Math/abs (- %1 %2)) [x0 y0] [x1 y1]))))]
+                 (for [step (->> (map #(Math/abs (- %1 %2))
+                                      [x0 y0] [x1 y1])
+                                 (apply max)
+                                 inc
+                                 range)]
                    [(+ x0 (* step xstep))
                     (+ y0 (* step ystep))]))))
      (reduce (fn [map key]
