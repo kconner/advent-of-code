d20dd8b10e2a654cd9fc4b3b43adf4d8ffdb28b7
Kevin Conner <connerk@gmail.com>
2021-12-06 13:03:37 -0500

    Day 6 part 2

---
+++ 2021/6b.clj
@@ -0,0 +1,9 @@
+(->> (clojure.string/split (slurp "6.txt") #",")
+     frequencies
+     (reduce-kv (fn [cs i c] (assoc cs (Integer. i) c))
+                (apply vector (repeat 9 0)))
+     (iterate (fn [[c0 c1 c2 c3 c4 c5 c6 c7 c8]]
+                [c1 c2 c3 c4 c5 c6 (+ c0 c7) c8 c0]))
+     (drop 256)
+     first
+     (reduce +))
