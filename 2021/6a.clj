0cf525f5bebcf6df44e272060b13caa0783ec143
Kevin Conner <connerk@gmail.com>
2021-12-06 12:57:03 -0500

    Go briefer

--- 2021/6a.clj
+++ 2021/6a.clj
@@ -1,15 +1,9 @@
 (->> (clojure.string/split (slurp "6.txt") #",")
      frequencies
-     (reduce-kv (fn [coll key count]
-                  (assoc coll (Integer. key) count))
+     (reduce-kv (fn [cs i c] (assoc cs (Integer. i) c))
                 (apply vector (repeat 9 0)))
-     (iterate (fn [counts]
-                (mapv (fn [n]
-                        (case n
-                          6 (+ (counts 0) (counts 7))
-                          8 (counts 0)
-                          (counts (inc n))))
-                      (range 9))))
+     (iterate (fn [[c0 c1 c2 c3 c4 c5 c6 c7 c8]]
+                [c1 c2 c3 c4 c5 c6 (+ c0 c7) c8 c0]))
      (drop 80)
      first
      (reduce +))
