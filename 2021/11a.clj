ae625fa7215742b52b02b56d6f623e8dd8677e7d
Kevin Conner <connerk@gmail.com>
2021-12-11 11:34:56 -0500

    Tidy

--- 2021/11a.clj
+++ 2021/11a.clj
@@ -1,53 +1,48 @@
 (require '[clojure.string :as string])
 (require '[clojure.set :as set])
 
+(def ordinates (range 10))
+
+(def all-cells (for [r ordinates c ordinates] [r c]))
+
 (def neighboring-ordinates
   (memoize
-   (fn [i]
-     (set/intersection #{(dec i) i (inc i)}
-                       (set (range 10))))))
+   (fn [i] (set/intersection #{(dec i) i (inc i)} (set ordinates)))))
 
 (def neighbors
   (memoize
-   (fn [[r c]]
-     (set (for [ar (neighboring-ordinates r)
-                ac (neighboring-ordinates c)] [ar ac])))))
+   (fn [[r c]] (set (for [ar (neighboring-ordinates r)
+                          ac (neighboring-ordinates c)] [ar ac])))))
 
 (defn rays-from-border [flashed-cells border-cells]
   (->> border-cells
        (mapcat (fn [cell] (set/difference (neighbors cell) flashed-cells)))
        frequencies))
 
+(defn absorb [rows rays]
+  (reduce-kv (fn [[rows flashing-cells] cell ray-count]
+               (let [value (+ (get-in rows cell) ray-count)]
+                 [(assoc-in rows cell value)
+                  (if (<= value 9) flashing-cells (conj flashing-cells cell))]))
+             [rows #{}]
+             rays))
+
 (defn flash [rows flashed-cells border-cells]
   (let [rays (rays-from-border flashed-cells border-cells)]
     (if (empty? rays) rows
-        (let [[rows frontier-cells]
-              (reduce-kv (fn [[rows frontier-cells] cell ray-count]
-                           (let [value (+ (get-in rows cell) ray-count)]
-                             [(assoc-in rows cell value)
-                              (if (<= value 9) frontier-cells
-                                  (conj frontier-cells cell))]))
-                         [rows #{}]
-                         rays)]
-          (flash rows
-                 (set/union flashed-cells frontier-cells)
-                 frontier-cells)))))
+        (let [[rows frontier] (absorb rows rays)]
+          (flash rows (set/union flashed-cells frontier) frontier)))))
+
+(def ambient-rays (frequencies all-cells))
 
 (defn step [rows flash-count]
-  (let [[rows flashed-cells]
-        (reduce (fn [[rows flashed-cells] cell]
-                  (let [value (inc (get-in rows cell))]
-                    [(assoc-in rows cell value)
-                     (if (<= value 9) flashed-cells
-                         (conj flashed-cells cell))]))
-                [rows #{}]
-                (for [r (range 10) c (range 10)] [r c]))]
+  (let [[rows flashed-cells] (absorb rows ambient-rays)]
     (reduce (fn [[rows flash-count] cell]
               (if (<= (get-in rows cell) 9)
                 [rows flash-count]
                 [(assoc-in rows cell 0) (inc flash-count)]))
             [(flash rows flashed-cells flashed-cells) flash-count]
-            (for [r (range 10) c (range 10)] [r c]))))
+            all-cells)))
 
 (def rows (mapv (partial mapv #(compare % \0))
                 (string/split-lines (slurp "11.txt"))))
