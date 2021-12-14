3a63e831b7c5fad8c1fe8401105557945906ec23
Kevin Conner <connerk@gmail.com>
2021-12-13 21:36:25 -0500

    Commentary

--- 2021/13a.clj
+++ 2021/13a.clj
@@ -1,8 +1,10 @@
 (require '[clojure.string :as string])
 
+; A point is the integer values of the text on either side of its line's comma.
 (defn point-for-line [line]
   (mapv #(Integer. %) (string/split line #",")))
 
+; A fold function flips points around a given line's vertex on its stated axis.
 (defn fold-for-line [line]
   (let [parts (string/split line #" |=")
         axis (case (parts 2) "x" 0 "y" 1)
@@ -11,17 +13,25 @@
     (fn [point]
       (update point axis #(if (< % vertex) % (- doubled-vertex %))))))
 
-(def text
+; The paragraphs are the file's text split by double newlines.
+(def paragraphs
   (string/split (slurp "13.txt") #"\n\n"))
 
+; The total fold operation is the fold function for the first line
+; of the second paragraph.
 (def folds
-  (->> (second text)
+  (->> (second paragraphs)
        string/split-lines
        first
        fold-for-line))
 
-(->> (first text)
-     string/split-lines
-     (map (comp folds point-for-line))
-     set
-     count)
+; The folded points are the unique results of the fold operation
+; on each point given lines of the first paragraph.
+(def folded-points
+  (->> (first paragraphs)
+       string/split-lines
+       (map (comp folds point-for-line))
+       set))
+
+; Count em.
+(count folded-points)
--- 2021/13b.clj
+++ 2021/13b.clj
@@ -1,8 +1,10 @@
 (require '[clojure.string :as string])
 
+; A point is the integer values of the text on either side of its line's comma.
 (defn point-for-line [line]
   (mapv #(Integer. %) (string/split line #",")))
 
+; A fold function flips points around a given line's vertex on its stated axis.
 (defn fold-for-line [line]
   (let [parts (string/split line #" |=")
         axis (case (parts 2) "x" 0 "y" 1)
@@ -11,29 +13,40 @@
     (fn [point]
       (update point axis #(if (< % vertex) % (- doubled-vertex %))))))
 
-(def text
+; The paragraphs are the file's text split by double newlines.
+(def paragraphs
   (string/split (slurp "13.txt") #"\n\n"))
 
+; The total fold operation is the composition of the fold functions
+; for each line of the second paragraph.
 (def folds
-  (->> (second text)
+  (->> (second paragraphs)
        string/split-lines
        reverse
        (map fold-for-line)
        (apply comp)))
 
+; The folded points are the unique results of the fold operation
+; on each point given lines of the first paragraph.
 (def folded-points
-  (->> (first text)
+  (->> (first paragraphs)
        string/split-lines
        (map (comp folds point-for-line))
        set))
 
+; The dimensions are the inclusive ranges from 0 to each
+; maximum value among all points, per axis.
 (def dimensions
-  (reduce (partial mapv max) folded-points))
+  (mapv (comp range inc)
+        (reduce (partial mapv max) folded-points)))
 
+; The lines are joined strings per Y, which are joined characters per X,
+; in which the character is visible when the point is among the folded points.
 (def lines
-  (for [r (range (inc (second dimensions)))]
+  (for [y (second dimensions)]
     (string/join
-     (for [c (range (inc (first dimensions)))]
-       (if (folded-points [c r]) \# \space)))))
+     (for [x (first dimensions)]
+       (if (folded-points [x y]) \# \space)))))
 
+; Print em.
 (println (string/join \newline lines))
