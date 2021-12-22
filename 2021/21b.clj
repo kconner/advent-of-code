bcc79ecb472a622d2e601cbb079e8e88bdb05ae1
Kevin Conner <connerk@gmail.com>
2021-12-21 21:43:32 -0500

    Store player states in maps

--- 2021/21b.clj
+++ 2021/21b.clj
@@ -12,18 +12,20 @@
        ((if (<= depth 2) pmap map)
         (fn [[roll-sum frequency]]
           (let [universes (* universes frequency)
-                [score place] (player-states player)
+                {score :score place :place} (player-states player)
                 place (mod (+ place roll-sum) 10)
                 score (+ score (place-scores place))]
             (if (<= 21 score)
               {player universes}
               (search universes
-                      (assoc player-states player [score place])
+                      (assoc player-states player {:score score :place place})
                       (- 1 player)
                       (inc depth))))))
        (reduce (partial merge-with +))))
 
 (time (let [player-states
-            (mapv (fn [line] [0 (Integer. (last (string/split line #" ")))])
+            (mapv (fn [line]
+                    {:score 0
+                     :place (Integer. (last (string/split line #" ")))})
                   (string/split-lines (slurp "21.txt")))]
         (apply max (vals (search 1 player-states 0 0)))))
