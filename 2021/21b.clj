f28a8cd20c8c141a2cda670ca78fa5622449bc2a
Kevin Conner <connerk@gmail.com>
2021-12-21 20:59:57 -0500

    Handle less data when taking a turn

--- 2021/21b.clj
+++ 2021/21b.clj
@@ -2,11 +2,9 @@
 
 (def place-scores [10 1 2 3 4 5 6 7 8 9])
 
-(defn take-turn [game whose-turn roll-sum]
-  (update game whose-turn
-          (fn [[score place]]
-            (let [place (mod (+ place roll-sum) 10)]
-              [(+ score (place-scores place)) place]))))
+(defn take-turn [[score place] roll-sum]
+  (let [place (mod (+ place roll-sum) 10)]
+    [(+ score (place-scores place)) place]))
 
 (def roll-sum-frequencies
   (->> (let [rolls (range 1 4)]
@@ -19,12 +17,13 @@
        ((if (<= depth 2) pmap map)
         (fn [[roll-sum frequency]]
           (let [universes (* universes frequency)
-                [[score-0 _] [score-1 _] :as game]
-                (take-turn game whose-turn roll-sum)]
-            (cond (<= 21 score-0) [universes 0]
-                  (<= 21 score-1) [0 universes]
-                  :else (search universes game
-                                (- 1 whose-turn) (inc depth))))))
+                [score place :as state] (take-turn (game whose-turn) roll-sum)]
+            (if (<= 21 score)
+              (if (zero? whose-turn) [universes 0] [0 universes])
+              (search universes
+                      (assoc game whose-turn state)
+                      (- 1 whose-turn)
+                      (inc depth))))))
        (reduce (fn [[sv0 sv1] [v0 v1]] [(+ sv0 v0) (+ sv1 v1)]))))
 
 (time (apply
