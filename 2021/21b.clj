11ef25f97cb44b7a8d0f4da00669b58ec046b271
Kevin Conner <connerk@gmail.com>
2021-12-21 20:42:03 -0500

    Don't carry whose turn it is in game state

--- 2021/21b.clj
+++ 2021/21b.clj
@@ -2,9 +2,8 @@
 
 (def place-scores [10 1 2 3 4 5 6 7 8 9])
 
-(defn take-turn [[universes whose-turn player-states] roll-sum frequency]
+(defn take-turn [[universes player-states] whose-turn roll-sum frequency]
   [(* universes frequency)
-   (- 1 whose-turn)
    (update player-states whose-turn
            (fn [[score place]]
              (let [place (mod (+ place roll-sum) 10)]
@@ -16,21 +15,22 @@
        (map (partial apply +))
        frequencies))
 
-(defn search [game depth]
+(defn search [game whose-turn depth]
   (->> roll-sum-frequencies
        ((if (<= depth 2) pmap map)
         (fn [[roll-sum frequency]]
-          (let [[universes _ [[score-0 _] [score-1 _]] :as game]
-                (take-turn game roll-sum frequency)]
+          (let [[universes [[score-0 _] [score-1 _]] :as game]
+                (take-turn game whose-turn roll-sum frequency)]
             (cond (<= 21 score-0) [universes 0]
                   (<= 21 score-1) [0 universes]
-                  :else (search game (inc depth))))))
+                  :else (search game (- 1 whose-turn) (inc depth))))))
        (reduce (fn [[sv0 sv1] [v0 v1]] [(+ sv0 v0) (+ sv1 v1)]))))
 
 (time (apply
        max
        (search
-        [1 0 (mapv (fn [line]
-                     [0 (Integer. (last (string/split line #" ")))])
-                   (string/split-lines (slurp "21.txt")))]
+        [1 (mapv (fn [line]
+                   [0 (Integer. (last (string/split line #" ")))])
+                 (string/split-lines (slurp "21.txt")))]
+        0
         0)))
