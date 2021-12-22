90d533ba6ff8f3db12a456979d59ae1db1efc653
Kevin Conner <connerk@gmail.com>
2021-12-21 21:28:23 -0500

    Bring movement and scoring into search function

--- 2021/21b.clj
+++ 2021/21b.clj
@@ -2,10 +2,6 @@
 
 (def place-scores [10 1 2 3 4 5 6 7 8 9])
 
-(defn take-turn [[score place] roll-sum]
-  (let [place (mod (+ place roll-sum) 10)]
-    [(+ score (place-scores place)) place]))
-
 (def roll-sum-frequencies
   (->> (let [rolls (range 1 4)] (for [a rolls b rolls c rolls] [a b c]))
        (map (partial apply +))
@@ -16,11 +12,13 @@
        ((if (<= depth 2) pmap map)
         (fn [[roll-sum frequency]]
           (let [universes (* universes frequency)
-                [score _ :as state] (take-turn (player-states player) roll-sum)]
+                [score place] (player-states player)
+                place (mod (+ place roll-sum) 10)
+                score (+ score (place-scores place))]
             (if (<= 21 score)
               (assoc [0 0] player universes)
               (search universes
-                      (assoc player-states player state)
+                      (assoc player-states player [score place])
                       (- 1 player)
                       (inc depth))))))
        (reduce (partial mapv +))))
