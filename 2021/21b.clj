d8dcf373602c736779695750c650619e9c21a8ec
Kevin Conner <connerk@gmail.com>
2021-12-21 20:09:13 -0500

    Use DFS

--- 2021/21b.clj
+++ 2021/21b.clj
@@ -16,30 +16,21 @@
        (map (partial apply +))
        frequencies))
 
-(def win-at 21)
+(defn search [game depth]
+  (->> roll-sum-frequencies
+       ((if (<= depth 1) pmap map)
+        (fn [[roll-sum frequency]]
+          (let [[universes _ [[score-0 _] [score-1 _]] :as game]
+                (take-turn game roll-sum frequency)]
+            (cond (<= 21 score-0) [universes 0]
+                  (<= 21 score-1) [0 universes]
+                  :else (search game (inc depth))))))
+       (reduce (fn [[sv0 sv1] [v0 v1]] [(+ sv0 v0) (+ sv1 v1)]))))
 
-(defn take-turns [[victories-0 victories-1 games]]
-  (let [games (for [game games
-                    [roll-sum frequency] roll-sum-frequencies]
-                (take-turn game roll-sum frequency))
-        {new-victories-0 true games false}
-        (group-by (fn [[_ _ [[score-0 _] [_ _]]]] (<= win-at score-0)) games)
-        {new-victories-1 true games false}
-        (group-by (fn [[_ _ [[_ _] [score-1 _]]]] (<= win-at score-1)) games)]
-    [(apply + victories-0 (map first new-victories-0))
-     (apply + victories-1 (map first new-victories-1))
-     games]))
-
-(def -initial-game [1 0 (mapv (fn [line]
-                                [0 (Integer. (last (string/split line #" ")))])
-                              (string/split-lines (slurp "21.txt")))])
-(def -initial-state [0 0 [-initial-game]])
-
-(take-turns -initial-state)
-
-(time (subvec (first (drop 100 (iterate (fn [state] (time (take-turns state))) -initial-state))) 0 2))
-
-; this is taking way too long to run.
-; step 9 only took twice as long as step 8, but it used 7GB by then.
-; i could pmap on the roll sum frequencies to partition the games.
-; i could try DFS with frequencies.
+(time (apply
+       max
+       (search
+        [1 0 (mapv (fn [line]
+                     [0 (Integer. (last (string/split line #" ")))])
+                   (string/split-lines (slurp "21.txt")))]
+        0)))
